66af4d431431d6a81c2ed29b9db8ac5766e45851
ans9611 <ans9611@gmail.com>
2021-09-15 21:28:31 -0400

    added rating display to star

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import { Link } from 'react-router-dom'
 
 function Product ({ product }) {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
-          <div className="my=3">
-            {product.rating} from {product.numReviews} reviews
+          <div className="my-3">
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
           </div>
         </Card.Text>
 
         <Card.Text as="h3">
                     ${product.price}
         </Card.Text>
-
       </Card.Body>
     </Card>
   )
