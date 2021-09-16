99a47e45117df94dfd87d663358433d54466e1ce
ans9611 <ans9611@gmail.com>
2021-09-16 13:56:55 -0400

    created product screen

--- src/products.js
+++ src/products.js
@@ -3,8 +3,7 @@ const products = [
     _id: '1',
     name: 'Option for Passive investor',
     image: '../public/images/Active.png',
-    description:
-      'Economic option for passive investor ',
+    description: 'Economic option for passive investor ',
     brand: 'GoldmanSocks',
     category: 'consulting',
     price: 99.99,
@@ -16,8 +15,7 @@ const products = [
     _id: '2',
     name: 'Trader',
     image: '../public/images/Trader.jpg',
-    description:
-      'Hire Top class trader',
+    description: 'Hire Top class trader',
     brand: 'GoldmanSocks',
     category: 'consulting',
     price: 599.99,
@@ -29,14 +27,26 @@ const products = [
     _id: '3',
     name: 'Premium',
     image: '../public/images/Active.jpg',
-    description:
-      'Hire Most Experienced Ivy Financial Advisors',
+    description: 'Hire Most Experienced Ivy Financial Advisors',
     brand: 'GoldmanSocks',
     category: 'consulting',
     price: 2999.99,
     countInStock: 5,
     rating: 3,
     numReviews: 12
-  }]
+  },
+  {
+    _id: '4',
+    name: 'Goldman Sachs Socks',
+    image: '../public/images/goldmanSocks.jpeg',
+    description: 'Hire Most Experienced Ivy Financial Advisors',
+    brand: 'GoldmanSocks',
+    category: 'consulting',
+    price: 2999.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12
+  }
+]
 
 export default products
