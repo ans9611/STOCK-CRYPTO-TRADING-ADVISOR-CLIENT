ddf4f343d7e825130d8536164de725dbaf3eadd6
ans9611 <ans9611@gmail.com>
2021-10-14 20:38:18 -0400

    updated settings

--- src/api/auth.js
+++ src/api/auth.js
@@ -33,7 +33,7 @@ export const signOut = (user) => {
     url: apiUrl + '/sign-out/',
     method: 'DELETE',
     headers: {
-      Authorization: `Bearer ${user.token}`
+      Authorization: `Token ${user.token}`
     }
   })
 }
@@ -43,7 +43,7 @@ export const changePassword = (passwords, user) => {
     url: apiUrl + '/change-password/',
     method: 'PATCH',
     headers: {
-      Authorization: `Bearer ${user.token}`
+      Authorization: `Token ${user.token}`
     },
     data: {
       passwords: {
